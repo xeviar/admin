cf3516212cce4dbe63c5a3dfadc4662a0a856424
WangWei@Ubuntu <wangw@garena.com>
2014-11-19 17:37:19 +0800

    llll

--- start.sh
+++ start.sh
@@ -3,5 +3,6 @@
 DIR=`dirname $0`
 cd $DIR
 echo $DIR
-#python manager.py   runserver 0.0.0.0:8080 >>$DIR/web.log 2>&1
-python manager.py   runserver 0.0.0.0:8080
+#python manage.py   runserver 0.0.0.0:8080 >>$DIR/web.log 2>&1
+python manage.py   runserver 0.0.0.0:8080
+#python manage.py runfcgi host=127.0.0.1 port=9090 pidfile=/tmp/admin.pid
